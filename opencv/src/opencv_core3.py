35ff00b3dbff29240f037039885247e624627a6c
李泉延 <liquanyan@liquanyandeMac-mini.local>
2025-09-13 09:54:35 +0800

    macos_cv

--- opencv/src/opencv_core3.py
+++ opencv/src/opencv_core3.py
@@ -1,24 +1,22 @@
 import cv2
 # 这个代码是opencv的基础部分
-# 下面的代码是图像的缩放
+# 下面的代码展示了修改图片上的某一个像素点
 ImagePath='opencv/image/image.png'
 Image=cv2.imread(ImagePath)
 # 检查图片是否正确加载
 if Image is None:
     print(f"错误：无法加载图片，请检查路径是否正确: {ImagePath}")
     exit()
-ImageSize=Image.size
-print(ImageSize)
-print("***********************")
-# 将图像缩放为100*100
-ResizeImage=cv2.resize(Image,(100,100))
-ResizeImageSize=ResizeImage.size
-print(ResizeImageSize)
-cv2.imshow("DisplayImage",ResizeImage)
+# 获取像素值
+PixelImage=Image[100,100]
+print(Image[100,100])
+# 修改像素值->将100,100修改成白色
+Image[100,100]=[255,255,255]
+cv2.imshow("DisplayImage",Image)
 key=cv2.waitKey(0)
 if key ==ord('s'):
     print("保存图像")
-    SaveImage=cv2.imwrite('opencv/output/image/ReszieImage.png',Image)
+    SaveImage=cv2.imwrite('opencv/output/image/PixelImage.png',Image)
 if key ==ord('q'):
     print("成功退出")
 
